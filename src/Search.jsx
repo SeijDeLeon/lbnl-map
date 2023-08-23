97859a2cdd3302847493284e4d9f54b178ffba2a
SeijDeLeon <sdeleon@berkeley.edu>
2023-08-23 16:22:12 -0700

    add first row of search inputs

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,12 +1,22 @@
 import Results from './Results.jsx';
 
 import { useState } from 'react';
+import { useDeferredValue } from 'react';
 
 export default function Search( {building, setBuilding} ) {
 
   const searchIcon = <svg className='self-center w-auto h-3/4' xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50"><path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"></path></svg>
 
+  const chevronDown = <svg className='self-center' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-compact-down" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67z"/> </svg>
 
+  const chevronUp = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-compact-up" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M7.776 5.553a.5.5 0 0 1 .448 0l6 3a.5.5 0 1 1-.448.894L8 6.56 2.224 9.447a.5.5 0 1 1-.448-.894l6-3z"/> </svg>
+
+
+  const deferredBuilding = useDeferredValue(building); //ensures that keystrokes are prioritized over searches
+
+  const [wall, setWall] = useState(false);
+  const [floor, setFloor] = useState(false);
+  const [wallAndFloor, setWallAndFloor] = useState(false);
 
   return(
     <div className='border-red-500 border-solid border-2'>
@@ -19,7 +29,35 @@ export default function Search( {building, setBuilding} ) {
           </div>
         </search>
       </div>
-      <Results />
+      <section className='border-2 border-solid border-slate-300 block'>
+        <h2>Search for Structural Calculations</h2>
+        <div className='flex justify-around'>
+          <button className='flex'> Equipment Type {chevronDown}</button>
+          <label>
+            Floor
+            <select>
+              <option value='all'>All</option>
+              <option value='1'>1st</option>
+              <option value='2'>2nd</option>
+              <option value='3'>3rd</option>
+              <option value='4'>4th</option>
+              <option value='5'>5th</option>
+              <option value='6'>6th</option>
+              <option value='roof'>Roof</option>
+            </select>
+          </label>
+          <div className='flex'>
+            <p>Attachment</p>
+            <label><input type='checkbox' defaultChecked={false} name='wall' onChange={()=> setWall(!wall)}/> Wall </label>
+            <label> <input type='checkbox' defaultChecked={false} name='floor' onChange={()=> setFloor(!floor)}/>Floor</label>
+            <label> <input type='checkbox' defaultChecked={false} name='wallAndFloor' onChange={()=> setWallAndFloor(!wallAndFloor)}/>Wall & Floor</label>
+
+          </div>
+        </div>
+        <div>inputs2</div>
+        <Results building={deferredBuilding}/>
+      </section>
+      <p>{wall ? 'yes wall' : 'no wall'}</p>
     </div>
   )
 }
